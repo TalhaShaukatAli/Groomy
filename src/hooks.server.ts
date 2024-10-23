fb0588f9a04627e186801640d6370d4f1a83129e
Ethan Henrickosn <henricksonethan@gmail.com>
2024-10-22 19:23:15 -0500

    Database Auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,21 @@
-import { connect } from "$lib/db/mongo";
+import { connect } from '$lib/db/mongo';
+import type { Handle } from '@sveltejs/kit';
 
 // Connect to MongoDB before starting the server
-connect().then(():void => {
-    console.log("MongoDB started");
-}).catch((e) => {
-    console.log("MongoDB failed to start");
-    console.log(e);
-});
+connect()
+	.then((): void => {
+		console.log('MongoDB started');
+	})
+	.catch((e) => {
+		console.log('MongoDB failed to start');
+		console.log(e);
+	});
+
+export const handle: Handle = async ({ event, resolve }) => {
+	if(event.url.pathname.startsWith("/home")){
+		console.log('sad');
+        return await resolve(event);
+	} else {
+        return await resolve(event);
+    }
+};
--- src/routes/(marketing)/login/+page.server.ts
+++ src/routes/(marketing)/login/+page.server.ts
@@ -1,37 +1,56 @@
 /** @type {import('./$types').Actions} */
 
 import { AddNewUser, GetUserByEmail } from '$lib/db/database';
+import { generateRandomString } from '@oslojs/crypto/random';
+import argon2 from "argon2";
 import type { newUser } from '$lib/types';
 import type { Actions } from './$types';
 
+import type { RandomReader } from '@oslojs/crypto/random';
+
+const random: RandomReader = {
+	read(bytes: Uint8Array): void {
+		crypto.getRandomValues(bytes);
+	}
+};
+
+
 export const actions = {
 	login: async ({ cookies, request}) => {
-		// TODO log the user in
         const data = await request.formData();
 		const email = data.get('email');
-		const password = data.get('password');
+		const password = <string>data.get('password');
 
         let user = await GetUserByEmail(email)
-        if (user == null){
 
+		if (user == null){
+			return undefined
 		} else {
-			
+			if (await argon2.verify(user.password, password)) {
+				console.log(password);
+			}
 		}
 
+		
+
+		let cookieID = generateRandomString(random, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 20);
+		cookies.set('sessionID', cookieID, { path: '/' });
 	},
 	create: async ({ cookies, request }) => {
-		// TODO register the user
         const data = await request.formData();
 		const email = data.get('email');
 		const password = data.get('password');
+		const passwordHash = await argon2.hash(password);
 		const firstName = data.get('firstName');
 		const lastName = data.get('lastName');
+
+		console.log(passwordHash)
 
         const newUserData:newUser = {
             firstName: firstName,
             lastName: lastName,
             email: email,
-            password: password
+            password: passwordHash
         }
 
         await AddNewUser(newUserData)
